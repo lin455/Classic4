fb9fc51eac4c3f7e9bd25d9f8bf9eb3d76480378
lin455 <jack08281113@gmail.com>
2021-09-10 20:17:31 +0800

    Initial Commit

---
+++ Classic4/Classic4App.swift
@@ -0,0 +1,17 @@
+//
+//  Classic4App.swift
+//  Classic4
+//
+//  Created by 暨大附中10 on 2021/9/10.
+//
+
+import SwiftUI
+
+@main
+struct Classic4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Classic4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Classic4
+//
+//  Created by 暨大附中10 on 2021/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
